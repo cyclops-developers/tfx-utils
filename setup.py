6111868e44b1167a62ab802244550e7453262dd1
Danilo Nunes <nunesdanilo@gmail.com>
2020-01-08 21:02:23 -0300

    fix setup.py package generation'

--- setup.py
+++ setup.py
@@ -21,18 +21,8 @@ URL = "https://github.com/laguro-developers/tfx-utils"
 # External dependencies
 # More info https://pythonhosted.org/setuptools/setuptools.html#declaring-dependencies
 REQUIREMENTS_EXTERNAL = [
-    'six>=1.10.0',
-    'bumpversion>=0.5.3',
-    'click>=3.3',
-    'jupyter>=1.0.0',
-    'pep8>=1.7.0',
-    'virtualenv>=15.0.1',
-    'pytest>=2.6.4',
-    'pytest-cov>=1.8.1',
-    'mock>=2.0.0',
-    'tox>=2.2.0',
-    'pytest-watch>=4.1.0',
-    'jsonschema>=2.5.1',
+    'Click==7.0',
+    'tfx>=0.15.0',
 ]
 
 # Test dependencies
@@ -71,32 +61,6 @@ def _get_version():
     return version
 
 
-class Tox(TestCommand):
-    """Run the test cases using TOX command."""
-    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.tox_args = None
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # Import here, cause outside the eggs aren't loaded
-        import tox
-        import shlex
-        args = self.tox_args
-        if args:
-            args = shlex.split(self.tox_args)
-        else:
-            # Run all tests by default
-            args = ['-c', os.path.join(os.path.dirname(__file__), 'tox.ini'), 'tests']
-        errno = tox.cmdline(args=args)
-        sys.exit(errno)
-
 setup(
     name=PACKAGE_NAME,
     version=_get_version(),
@@ -114,5 +78,4 @@ setup(
     tests_require=REQUIREMENTS_TESTS,
     dependency_links=DEPENDENCY_LINKS_EXTERNAL,
     scripts=SCRIPTS,
-    cmdclass={'test': Tox},
 )
